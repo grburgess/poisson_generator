72d3def8641bd26221be5ff07d6e24d0c89a271a
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-12-18 09:49:19 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+    
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+from distutils.core import setup
+
+
+setup(name='poisson_generator',
+      version='0.1',
+      description='Poisson generator for time series',
+      author='J. Michael Burgess',
+      author_email='jmichaelburgess@gmail.com',
+      packages=['poisson_generator'],
+     )
